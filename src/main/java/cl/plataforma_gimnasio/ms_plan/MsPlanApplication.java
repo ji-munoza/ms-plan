9495331fa36a82ad839a6ceffdf87686b81b882d
ji-munoza <ji.munoza@duocuc.cl>
2026-05-16 22:54:43 -0400

    añadida conexion con eureka

--- src/main/java/cl/plataforma_gimnasio/ms_plan/MsPlanApplication.java
+++ src/main/java/cl/plataforma_gimnasio/ms_plan/MsPlanApplication.java
@@ -2,7 +2,9 @@ package cl.plataforma_gimnasio.ms_plan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsPlanApplication {
 

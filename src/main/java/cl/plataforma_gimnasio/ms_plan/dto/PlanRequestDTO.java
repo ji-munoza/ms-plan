42850c87cb96086f19049a7547a4c928e353d90a
ji-munoza <ji.munoza@duocuc.cl>
2026-05-17 14:10:50 -0400

    mejoradas validaciones de dto

--- src/main/java/cl/plataforma_gimnasio/ms_plan/dto/PlanRequestDTO.java
+++ src/main/java/cl/plataforma_gimnasio/ms_plan/dto/PlanRequestDTO.java
@@ -1,28 +1,31 @@
 package cl.plataforma_gimnasio.ms_plan.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlanRequestDTO {
 
     @NotBlank(message = "El nombre del plan es obligatorio.")
-    @Size(max = 50, message = "El nombre del plan no puede superar los 50 caracteres.")
+    @Size(min = 3, max = 50, message = "El nombre del plan debe tener entre 3 y 50 caracteres.")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9.\\s]+$", message = "El nombre del plan solo puede contener letras, numeros y espacios.")
     private String nombre;
 
     @NotNull(message = "La duración en días es obligatoria.")
-    @Positive(message = "La duración en días debe ser un número mayor a cero.")
+    @Min(value = 1, message = "La duracion minima debe ser de al menos 1 dia.")
+    @Max(value = 365, message = "La duracion maxima no puede superar los 365 dias (1 año).")
     private Integer duracion;
 
     @NotNull(message = "El valor del plan es obligatorio.")
     @Min(value = 5000, message = "El valor mínimo de un plan debe ser de $5.000.")
+    @Max(value = 1000000, message = "El valor maximo de un plan no puede superar el de $1.000.000.")
     private Integer valor;
 
     @NotBlank(message = "Los beneficios del plan son obligatorios.")
-    @Size(max = 255, message = "Los beneficios no pueden superar los 255 caracteres.")
+    @Size(min = 5, max = 200, message = "Los beneficios deben tener entre 5 y 200 caracteres.")
     private String beneficios;
 }
